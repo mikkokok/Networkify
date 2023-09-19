d12f0b7f58dc271ea2976ac2ab51e6b8cf31b745
Mikko <mikko.kokkonen@outlook.com>
2023-09-19 19:54:37 +0300

    Initial commit

---
+++ Models/UserNetworkInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Networkify.Models
+{
+    public class UserNetworkInfo
+    {
+        public string ip { get; set; }
+        public int port { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Networkify.Models;
+
+namespace Networkify.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public UserNetworkInfo UserNetworkInfo { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            UserNetworkInfo = new UserNetworkInfo
+            {
+                ip = HttpContext.Connection.RemoteIpAddress?.ToString(),
+                port = HttpContext.Connection.RemotePort
+            };
+
+        }
+    }
+}
